3895b0b9e1b13f61264edba5d5b86d591d196500
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-21 17:15:20 +0100

    hot fix

--- models/amenity.py
+++ models/amenity.py
@@ -9,5 +9,4 @@ from models.place import place_amenity
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', back_populates='amenities',
-                                   secondary=place_amenity)
+    place_amenities = relationship('Place', secondary=place_amenity)
