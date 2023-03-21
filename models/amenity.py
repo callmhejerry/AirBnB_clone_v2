22012ca7cdb899592e3363219ca380f2aa1a9564
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-21 18:55:22 +0100

    hot fix

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+import os
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     place_amenities = relationship('Place', back_populates='amenities',
                                    secondary=place_amenity)
