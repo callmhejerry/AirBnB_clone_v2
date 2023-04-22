a73acb68f275826ee9d737d29f6fb921128dbfde
Apple <apple@MacBook-Pro.local>
2023-04-22 23:59:59 +0100

    style fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+'''Hello world route'''
 
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_page():
     '''return hello hbnb'''
     return "Hello HBNB!"
 
-app.run('0.0.0.0', debug=True)
+
+app.run('0.0.0.0')
