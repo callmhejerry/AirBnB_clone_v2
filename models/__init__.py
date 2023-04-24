f6235800fd665f76202d116f708fbd64960d4749
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-25 00:26:41 +0100

    hot fix

--- models/__init__.py
+++ models/__init__.py
@@ -8,14 +8,16 @@ from models.place import Place
 from models.review import Review
 from models.user import User
 from models.state import State
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
 
 storage = None
+
 if getenv("HBNB_TYPE_STORAGE") == 'db':
-    from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload()
 
