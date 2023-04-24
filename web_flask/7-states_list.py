d4d73e77b0c55fe8f89311239a36371e09e24d9e
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-24 18:48:53 +0100

    added docstrings

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""
-A script that starts a flask application
-and displays a lists of all states
-"""
+""" Starts a Flask application related to HBNB. """
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
 
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """removes the current SQLAlchemy session"""
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -16,12 +20,6 @@ def states_list():
     """Displays a list of states"""
     all_states = storage.all(State).values()
     return render_template('7-states_list.html', states=all_states)
-
-
-@app.teardown_appcontext
-def tear_down(exception):
-    """removes the current SQLAlchemy session"""
-    storage.close()
 
 
 if __name__ == "__main__":
