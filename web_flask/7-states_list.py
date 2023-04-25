15ae30a0df1ed0edfa5789b894c2d030340db852
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-25 10:50:05 +0100

    solved the docstring issue

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """ Starts a Flask application related to HBNB. """
 
-from flask import Flask, render_template
-from models import storage
-from models.state import State
-
-app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def tear_down(exception):
-    """removes the current SQLAlchemy session"""
-    storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a list of states"""
-    all_states = storage.all(State).values()
-    return render_template('7-states_list.html', states=all_states)
-
-
 if __name__ == "__main__":
+    from flask import Flask, render_template
+    from models import storage
+    from models.state import State
+
+    app = Flask(__name__)
+
+    @app.teardown_appcontext
+    def tear_down(exception):
+        """removes the current SQLAlchemy session"""
+        storage.close()
+
+    @app.route('/states_list', strict_slashes=False)
+    def states_list():
+        """Displays a list of states"""
+        all_states = storage.all(State).values()
+        return render_template('7-states_list.html', states=all_states)
+
     app.run('0.0.0.0', port=5000)
